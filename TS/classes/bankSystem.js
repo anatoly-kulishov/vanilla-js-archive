54cb176d8fafea19cbfd93da94473fcdd58c8b70
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-05-16 11:22:06 +0300

    1.1.9

--- TS/classes/bankSystem.js
+++ TS/classes/bankSystem.js
@@ -9,10 +9,6 @@ var Bank = /** @class */ (function () {
         });
     }
     Bank.prototype.transfer = function (account1, account2, money) {
-        if (!this.dictionary.hasOwnProperty(account1) || this.dictionary[account1] < money)
-            return false;
-        if (!this.dictionary.hasOwnProperty(account2))
-            return false;
         return this.deposit(account2, money) && this.withdraw(account1, money);
     };
     Bank.prototype.deposit = function (account, money) {
@@ -27,9 +23,15 @@ var Bank = /** @class */ (function () {
         this.dictionary[account] = this.dictionary[account] - money;
         return true;
     };
+    Bank.prototype.logAllBalance = function () {
+        console.log(this.dictionary);
+    };
     return Bank;
 }());
 var bank = new Bank([10, 100, 20, 50, 30]);
-console.log('Withdraw:', bank.withdraw(3, 50));
-console.log('Deposit:', bank.deposit(5, 100));
-console.log('Transfer:', bank.transfer(2, 1, 50));
+bank.logAllBalance(); // { '1': 10, '2': 100, '3': 20, '4': 50, '5': 30 }
+console.log('Withdraw:', bank.withdraw(3, 50)); // false
+console.log('Withdraw:', bank.withdraw(3, 5)); // true
+console.log('Deposit:', bank.deposit(5, 100)); // true
+console.log('Transfer:', bank.transfer(2, 1, 50)); // true
+bank.logAllBalance(); // { '1': 60, '2': 50, '3': 15, '4': 50, '5': 130 }
