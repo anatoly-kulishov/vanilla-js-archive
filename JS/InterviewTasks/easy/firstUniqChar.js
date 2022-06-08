2ae8e87f8e08becb98346886136121f221385de5
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-06-08 11:11:01 +0300

    1.2.8

---
+++ JS/InterviewTasks/easy/firstUniqChar.js
@@ -0,0 +1,27 @@
+const testCase = require("../../Helper/testCase");
+
+let firstUniqChar = (s) => {
+  let map = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    let current = s[i];
+
+    if (map.has(current)) {
+      map.set(current, map.get(current) + 1)
+    } else {
+      map.set(current, 1);
+    }
+  }
+
+  for (let i = 0; i < map.length; i++) {
+    if (map.get(s[i] === 1)) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+testCase(firstUniqChar('leetcode'), 0, true);
+testCase(firstUniqChar('loveleetcode'), 2);
+testCase(firstUniqChar('aabb'), -1);
