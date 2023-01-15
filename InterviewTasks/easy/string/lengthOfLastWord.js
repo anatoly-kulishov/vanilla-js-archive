449a8f97a573ce1928c799adbc6277db206e9a24
a.kulishov <a.kulishov@astondevs.ru>
2023-01-15 16:54:15 +0300

    1.5.8

---
+++ InterviewTasks/easy/string/lengthOfLastWord.js
@@ -0,0 +1,13 @@
+const testCase = require("../../../Helpers/testCase");
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = (s) => {
+    return s.trim().split(' ').at(-1).length;
+};
+
+testCase(lengthOfLastWord('Hello World'), 5);
+testCase(lengthOfLastWord('   fly me   to   the moon  '), 4);
+testCase(lengthOfLastWord('luffy is still joyboy'), 6);
