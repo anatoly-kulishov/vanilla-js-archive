a56c04267ce2ce5e98df31f8a1d2125a448f305b
a.kulishov <a.kulishov@astondevs.ru>
2023-04-20 12:58:17 +0300

    1.7.1

--- InterviewDraft/Yandex/hashTable/singleNumber.js
+++ InterviewDraft/Yandex/hashTable/singleNumber.js
@@ -25,6 +25,26 @@ const singleNumber = (nums) => {
 	return uniqSum * 2 - numSum;
 };
 
+/**
+ * @param nums
+ * @returns {*|string}
+ */
+const singleNumber2 = (nums) => {
+	const map = {};
+
+	if(nums.length <= 1) return nums[0];
+
+	for(let num of nums) {
+		if(map[num] !== undefined) {
+			delete map[num];
+		} else {
+			map[num] = 1;
+		}
+	}
+
+	return Object.keys(map)[0];
+};
+
 testCase(singleNumber([4, 1, 2, 1, 2]), 4, true, 'singleNumber');
 testCase(singleNumber([2, 2, 1]), 1);
 testCase(singleNumber([1]), 1);
