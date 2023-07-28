d4d2416fb4936197604bcc16bd21215357b904c8
a.kulishov <a.kulishov@astondevs.ru>
2023-07-28 08:45:57 +0300

    1.7.9

--- InterviewDraft/Aston/Альфа-банк/countVowels.js
+++ InterviewDraft/Aston/Альфа-банк/countVowels.js
@@ -3,7 +3,6 @@
  * Важно отметить, что регулярное выражение [aeiouаеёиоуыюя] указано с флагами gi, чтобы учесть гласные буквы в разных регистрах (заглавных и строчных).
  * Второе решение использует метод includes(), чтобы проверить, содержится ли символ в строке с гласными буквами vowels.
  */
-
 function countVowels(str) {
   const vowelsRegex = /[aeiouаеёиоуыюя]/gi; // Регулярное выражение для поиска гласных (буквы могут быть в разных регистрах)
   const matches = str.match(vowelsRegex);
@@ -13,6 +12,8 @@ function countVowels(str) {
 const text = "привет";
 const vowelCount = countVowels(text);
 console.log(vowelCount); // Output: 2
+
+/** ************************************************************************ */
 
 function countVowels2(str) {
   const vowels = "aeiouаеёиоуыюя"; // Гласные буквы
