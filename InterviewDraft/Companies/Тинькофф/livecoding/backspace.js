0a6d66107ec3d6c6c62e0d2c0f9e648294ab1a2d
anatoly.kulishov <anatolkulishov@yandex.ru>
2024-03-18 16:17:32 +0300

    2.1.5

---
+++ InterviewDraft/Companies/Тинькофф/livecoding/backspace.js
@@ -0,0 +1,36 @@
+// const backspace = (str1, str2) => {
+//     // code here...
+// }
+
+const backspace = (str1, str2) => {
+    const str1Arr = [];
+    const str2Arr = [];
+
+    for(let i = 0; i < str1.length; i++) {
+        let current = str1[i];
+
+        if(current === '#') {
+            str1Arr.pop();
+            continue;
+        }
+
+        str1Arr.push(current)
+    }
+
+    for(let i = 0; i < str2.length; i++) {
+        let current = str2[i];
+
+        if(current === '#') {
+            str2Arr.pop();
+            continue;
+        }
+
+        str2Arr.push(current)
+    }
+
+    return str1Arr.join('') === str2Arr.join('')
+}
+
+console.log(backspace('ab#d', 'ac#d')) // true (ad === ad)
+console.log(backspace('ab##', 'ab##')) // true (a === a)
+console.log(backspace('ab#d', 'ab#c')) // false (ad !== ac)
---
+++ InterviewDraft/Companies/Тинькофф/livecoding/searchByMatrix.js
@@ -0,0 +1,33 @@
+const matrix = [
+    [1,   2,   3],
+    [10,  20,  30],
+    [100, 200, 300]
+];
+
+const n = 20;
+
+console.log(search(matrix, n));
+
+const getValueByIndex = (matrix, index) => {
+    const w = matrix[0].length
+
+    const row = index / w;
+    const col = index % w;
+
+    return matrix[row][col]
+}
+
+function search(matrix, n) {
+    let start = 0;
+    let end = matrix.length * matrix[0].length;
+    let found = false
+    let position = -1;
+
+    while (found === false && start <= end) {
+        const middle = Math.floor(start + end / 2)
+
+        // code here
+    }
+
+    return;
+}
