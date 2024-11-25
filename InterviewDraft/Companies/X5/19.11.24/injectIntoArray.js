690b1bf578e3e1ac4ae4908d0a6bdaaf6f2fbb26
Anatoly Kulishov <anatoly.kulishov@external.tele2.ru>
2024-11-25 11:44:34 +0300

    2.6.3

--- InterviewDraft/Companies/X5/19.11.24/injectIntoArray.js
+++ InterviewDraft/Companies/X5/19.11.24/injectIntoArray.js
@@ -1,23 +1,18 @@
-// Нужно написать функцию, которая бы принимала на вход массив строк
-// и вставляла в него, привязываясь к конкретной строке, одну
-// или несколько новых
-// Функция должна уметь вставлять как до конкретной строки, так и после
+/**
+ * Необходимо реализовать функцию,
+ * которая принимает на вход массив строк и вставляет в него одну или несколько строк,
+ * привязываясь к существующим строкам в массиве.
+ *
+ * Вставка должна быть гибкой, с возможностью выбора места вставки — до или после указанной строки.
+ */
 
-const injectIntoArray = (
-    colList, cols
-    // colList: Array<string>,
-    // cols: Array<{
-    //     data: string[];
-    //     target: string;
-    //     isAfter: boolean;
-    // }>
-) => {
+const injectIntoArray = (colList, cols) => {
     for (let i = 0; i < cols.length; i++) {
         const {data, target, isAfter} = cols[i];
         const targetIndex = colList.indexOf(target);
 
         if (targetIndex !== -1) {
-            const insertIndex = isAfter ? targetIndex : targetIndex - 1;
+            const insertIndex = isAfter ? targetIndex + 1 : targetIndex;
             colList.splice(insertIndex, 0, ...data);
         } else {
             colList.push(...data);
